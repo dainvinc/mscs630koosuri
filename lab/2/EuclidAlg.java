f23520b3d2dd881d0ffa1f5e5be1a7b37f8e5e06
Vishal Koosuri <vishal.koosuri1@marist.edu>
2017-02-08 14:11:50 -0500

    Update EuclidAlg.java

--- lab/2/EuclidAlg.java
+++ lab/2/EuclidAlg.java
@@ -1,3 +1,13 @@
+/**
+ * file: EuclidAlg.java
+ * author: Vishal Koosuri
+ * course: MSCS 630
+ * lab: 2-part1
+ *due date: February 8
+ *
+ *This file demonstrates the simple Euclid Algorithm
+ *which is used to generate GCD of two numbers.
+ */
 import java.util.*;
 
 public class EuclidAlg {
