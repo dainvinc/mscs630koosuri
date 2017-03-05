0459676a9126f01bc2e6aed7576a0870385ecdeb
Vishal K <Vishal K>
2017-03-04 19:57:25 -0500

    Updated lab 3 - Prints the entire matrix now

--- lab/3/MatConvert.java
+++ lab/3/MatConvert.java
@@ -4,26 +4,32 @@ public class MatConvert {
    public static String convertToHex(int number) {
     return Integer.toHexString(number);
   }
+  
   public int[][] getHexMatP(char s, String p) {
     int[][] mat = new int[4][4];
-    char[] ch = new char[p.length()];
+    String str = p;
+    char c = '~';
+    char[] ch = new char[64];
     for(int i=0; i<p.length(); i++) {
       ch[i] = p.charAt(i);
-      System.out.println(ch[i]);
+      //System.out.println(ch[i]);
     }
-    for(int i=0; i<p.length(); i++){
-      for(int j=0; j<p.length(); j++) {
-        try {
-          while(i < 4) {
-            mat[i][j] = (int)ch[i];
-            i++;
-          }
-        } catch(ArrayIndexOutOfBoundsException e) {
-          
+    int n =0;
+    int k=0;
+    for(int j=0; j<16; j++) {
+      try {
+        while(n < 4) {
+          mat[n][j] = (int)ch[k];
+          if(ch[k] == 0)
+            mat[n][j] = (int)c;
+          //System.out.println("--------"+mat[n][j]);
+          n++;
+          k++;
         }
-      }
+        n = 0;
+      } catch(Exception e) {}
     }
-     for(int i=0; i<4; i++) {
+    for(int i=0; i<4; i++) {
       for(int j=0; j<4; j++) {
         System.out.print(convertToHex(mat[i][j]) +"  ");
       }
@@ -31,6 +37,7 @@ public class MatConvert {
     } 
     return mat; 
   }
+  
   public static void main(String[] args) {
     MatConvert m = new MatConvert();
     Scanner s = new Scanner(System.in);
